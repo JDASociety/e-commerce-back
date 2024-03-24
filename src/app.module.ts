a4307bf200a61f5c39ba2ea21989c6df76818b88
diegosiac <diego35502@hotmail.com>
2024-03-23 21:10:48 -0600

    Add users module and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
