81803f5eea97ed8b85950ef1c78abf839c069907
diegosiac <diego35502@hotmail.com>
2024-03-29 17:28:45 -0600

    Refactor user controller and service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,63 +1,10 @@
-import {
-  Controller,
-  Post,
-  Body,
-  HttpStatus,
-  HttpException,
-} from '@nestjs/common';
+import { Controller } from '@nestjs/common';
+
 import { UserService } from './user.service';
-import { User } from './interfaces';
-import { UserDto } from './dtos';
-import { ApiOperation, ApiBody } from '@nestjs/swagger';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('Usuarios')
 export class UserController {
   constructor(private userService: UserService) {}
-
-  @Post('register')
-  @ApiOperation({
-    summary: 'Registra de Usuario',
-    description: 'Registra un usuario en la aplicacion',
-  })
-  @ApiBody({
-    type: UserDto,
-    description: 'Product data',
-    examples: {
-      examples1: {
-        value: {
-          firstName: 'el m',
-          lastName: 'maria',
-          userName: 'elm',
-          email: 'elm@hotmail.com',
-          password: '123467',
-          image: '',
-        },
-      },
-    },
-  })
-  async register(@Body() userData: User) {
-    const user = await this.userService.createUser(userData);
-    if (!user) {
-      throw new HttpException(
-        'User registration failed',
-        HttpStatus.BAD_REQUEST,
-      );
-    }
-
-    return { message: 'User registered successfully.', user };
-  }
-
-  @Post('login')
-  async login(
-    @Body('email') email: string,
-    @Body('password') password: string,
-  ) {
-    const isValid = await this.userService.validateUser(email, password);
-
-    if (!isValid.ok) {
-      throw new HttpException(isValid.message, HttpStatus.UNAUTHORIZED);
-    }
-    // Aquí deberías generar y devolver un JWT o algún mecanismo de sesión
-    return { message: 'Login successful' };
-  }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,5 @@
-import { Injectable, ConflictException } from '@nestjs/common';
-import { User, Prisma } from '@prisma/client';
-import * as bcrypt from 'bcrypt';
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from '../prisma/prisma.service';
 
 @Injectable()
@@ -17,56 +16,5 @@ export class UserService {
     return this.prisma.user.findUnique({
       where: { userName },
     });
-  }
-
-  async createUser(data: Prisma.UserCreateInput): Promise<User> {
-    const { password, userName, email } = data;
-
-    const isExistEmail = await this.getUserByEmail(email);
-
-    if (isExistEmail) {
-      throw new ConflictException('Ya existe Un usuario con ese correo');
-    }
-
-    const isExistUserName = await this.getUserByUserName(userName);
-
-    if (isExistUserName) {
-      throw new ConflictException(
-        'Ya existe un usuario con ese nombre de usuario',
-      );
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    return this.prisma.user.create({
-      data: {
-        ...data,
-        password: hashedPassword,
-      },
-    });
-  }
-
-  async validateUser(email: string, password: string) {
-    const user = await this.prisma.user.findUnique({ where: { email } });
-
-    if (!user) {
-      return {
-        ok: false,
-        message: 'No se encontró un usuario con ese email',
-      };
-    }
-
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-
-    if (!isPasswordValid) {
-      return {
-        ok: false,
-        message: 'La contraseña no es válida',
-      };
-    }
-
-    return {
-      ok: true,
-    };
   }
 }
