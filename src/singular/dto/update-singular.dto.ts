de307d0144815f7b97f0b41d4095303fadc24ad6
diegosiac <diego35502@hotmail.com>
2024-03-29 17:28:39 -0600

    Delete unused Singular module, DTOs, controller, and service

--- src/singular/dto/update-singular.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateSingularDto } from './create-singular.dto';
-
-export class UpdateSingularDto extends PartialType(CreateSingularDto) {}
--- src/singular/singular.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { SingularService } from './singular.service';
-import { SingularController } from './singular.controller';
-
-@Module({
-  controllers: [SingularController],
-  providers: [SingularService],
-})
-export class SingularModule {}
