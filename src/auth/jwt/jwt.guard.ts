ed02705de4623dd606c7806197df2048a99c6abb
diegosiac <diego35502@hotmail.com>
2024-03-29 19:20:46 -0600

    Refactor JwtAuthGuard to handle authentication errors

--- src/auth/jwt/jwt.guard.ts
+++ src/auth/jwt/jwt.guard.ts
@@ -1,5 +1,33 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 
+import { HttpException, HttpStatus } from '@nestjs/common';
+
+export class AuthException extends HttpException {
+  constructor(public message: string) {
+    super(message, HttpStatus.UNAUTHORIZED);
+  }
+}
+
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  handleRequest(err, user, info) {
+    if (!err && user) {
+      return user;
+    }
+
+    if (!info) {
+      throw new AuthException('El JWT no ha sido proporcionado o es inválido');
+    }
+
+    if (info.name === 'TokenExpiredError') {
+      throw new AuthException('Tu token ha expirado');
+    }
+
+    if (info.name === 'JsonWebTokenError') {
+      throw new AuthException('El JWT es inválido');
+    }
+
+    throw err || new UnauthorizedException();
+  }
+}
