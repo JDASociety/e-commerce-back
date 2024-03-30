a86451107184962d02852e9953d79a367c90f82a
diegosiac <diego35502@hotmail.com>
2024-03-29 18:07:57 -0600

    Add bearer authentication to API

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,7 @@ async function bootstrap() {
     .setTitle('Ecommerce API')
     .setDescription('Api para el ecommerce JDASIAC')
     .setVersion('0.1')
+    .addBearerAuth()
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
