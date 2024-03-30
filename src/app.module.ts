df5b36a81a2dce6bc3d531f72698e9fb8d8b1f52
diegosiac <diego35502@hotmail.com>
2024-03-29 19:54:17 -0600

    Add new modules to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,11 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { SeedModule } from './seed/seed.module';
+import { AdminModule } from './admin/admin.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
   imports: [
@@ -10,6 +15,11 @@ import { AuthModule } from './auth/auth.module';
     }),
     UserModule,
     AuthModule,
+    ProductModule,
+    OrderModule,
+    SeedModule,
+    AdminModule,
+    PaymentModule,
   ],
 })
 export class AppModule {}
