905cfca512bd2a5bd564696690601ae07166b4a4
Abraham HC <hernandez.c.abraham.00@gmail.com>
2024-05-24 13:15:36 -0600

    ProductDto added

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,53 @@
-export class CreateProductDto {}
+import {
+  IsDate,
+  IsNumber,
+  IsPositive,
+  IsString,
+  IsUUID,
+  Min,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsUUID()
+  @IsString()
+  id: string;
+
+  @IsString()
+  @Min(1)
+  name: string;
+
+  @IsString()
+  @Min(1)
+  tag: string;
+
+  @IsString()
+  @Min(1)
+  description: string;
+
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsNumber()
+  @IsPositive()
+  @Min(0)
+  stock: number;
+
+  @IsString()
+  @Min(1)
+  category: string;
+
+  @IsString()
+  @Min(1)
+  ProductImage: string;
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
+
+  @IsString()
+  @Min(1)
+  slug: string;
+}
